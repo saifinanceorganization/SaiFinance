9a576b17063a5c07a3c76af599b7585fc85115a9
RiyaPatel2002 <03112002riyapatel@gmail.com>
2026-06-09 14:24:56 +0530

    add sitemap integration for improved site navigation and SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 import vue from "@astrojs/vue";
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [icon(), vue()],
+  site: 'https://saifinance.com.au',
+  integrations: [icon(), vue(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
